79ac2fa0d3e9df183b26dd697b3aa18da17d8740
Chad L <72481913+clookcode@users.noreply.github.com>
2020-10-10 11:52:41 -0400

    fixed white space issue see comments in code

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -1,13 +1,15 @@
 package prove04;
-
+// had a weird Tab issue with code seemed I put some wrong whitespace chars in my code
+// see https://intellij-support.jetbrains.com/hc/en-us/community/posts/207045165-Show-non-printable-characters-Tabs-
+// to turn on visibility of whitespace characters.
 public class Main {
 
     public static void main(String[] args) {
 	// Please make a change to my code. Thanks!
         System.out.println("Hello, my name is Chad");
-	    
+
 	//New line from Nathan Kempton
-	System.out.println("Hello Chad, my name is Nathan");
+        System.out.println("Hello Chad, my name is Nathan");
 
 	//New line from Chad
         System.out.println("Thank you Nathan!");
