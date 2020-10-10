d877ec13ae99eb03cf2c180cdcd065c748161fd3
nkempton <72485004+nkempton@users.noreply.github.com>
2020-10-10 08:28:16 -0600

    Update Main.java

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Chad");
+	    
+	//New line from Nathan Kempton
+	System.out.println("Hello Chad, my name is Nathan");
     }
 }
